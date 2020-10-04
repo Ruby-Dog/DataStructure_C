69dd799b9fce840b905d136cc2d9c8126d607c9b
RubyDog <65643745+Ruby-Dog@users.noreply.github.com>
2020-10-04 20:03:54 +0800

    Update 2_Array陣列-多項式應用.c

--- 2_Array陣列-多項式應用.c
+++ 2_Array陣列-多項式應用.c
@@ -12,7 +12,7 @@ void addPolys(double poly1[],double poly2[],double poly3[]);
 void subPolys(double poly1[],double poly2[],double poly3[]);
 void mutiPolys(double poly1[],double poly2[],double poly4[]);
 
-double max_doble_value(double a,double b);
+double max_double_value(double a,double b); 
 
 int main(void) {
     // 5x^4 + 4x^3 - 2x^2 + 7
@@ -31,11 +31,11 @@ int main(void) {
     printf("polynomial 2:\n");
     showPoly(poly2);
 
-//     //Doing addition of polynomial 1 and polynomial 2
-//     addPolys(poly1,poly2,poly3);
-//     //show result;
-//     printf("poly1 + poly2 = \n");
-//     showPoly(poly3);
+    //Doing addition of polynomial 1 and polynomial 2
+    addPolys(poly1,poly2,poly3);
+    //show result;
+    printf("poly1 + poly2 = \n");
+    showPoly(poly3);
 
 
 //     //Doing substraction of polynomial 1 and polynomial 2
@@ -53,7 +53,7 @@ int main(void) {
     return 0;
 }
 
-//------------------------------
+//-----------------------------------------------------
 
 /* show polynomial */
 
@@ -89,3 +89,46 @@ void showPoly(double poly[]){
 
     printf("\n");
 }
+
+
+//-----------------------------------------------------
+
+/*find maximun*/
+
+double max_double_value(double a,double b){
+    if(a>b){
+        return a;
+    }else{
+        return b;   //a < b 或 a == b 都回傳b 
+    }
+    
+}
+
+
+//-----------------------------------------------------
+
+/*polynomial 1 + polynomial 2*/
+
+void addPolys(double poly1[],double poly2[],double poly3[]){
+    int max_power = 0;
+    int n = 0;  //正在處理的項的該冪次數
+    max_power = (int)max_double_value(poly1[0],poly2[0]);
+    poly3[0]=max_power;
+    for( n=max_power ; n>=0 ; n--){
+        if(poly1[0]>=n && poly2[0]>=n){ //多項式的最高次項是不是大於現在正在處理的次項 
+                                        //如果是，代表該次項存在
+            poly3[max_power-n+1]= (poly1[(int)poly1[0]-n+1])+(poly2[(int)poly2[0]-n+1]); 
+            //陣列的imdex為整數 所以要用（int）強迫轉型
+            
+        }else if(poly1[0]>=n){
+            poly3[max_power-n+1]=poly1[(int)poly1[0]-n+1];
+        }else if(poly2[0]>=n){
+            poly3[max_power-n+1]=poly2[(int)poly2[0]-n+1];
+        }else{ 
+            poly3[max_power-n+1]=0.0;
+        }
+    }
+   
+    
+}
+
